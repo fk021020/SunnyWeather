ced0bc31141f55338ddf4de17d0bddfb4848f006
fk021020 <1984872513@qq.com>
2024-09-06 10:03:40 +0800

    修正

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -2,14 +2,16 @@ package com.sunnyweather.android.ui.weather
 
 import android.content.Context
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
@@ -19,36 +21,32 @@ import com.sunnyweather.android.R
 import com.sunnyweather.android.databinding.ActivityWeatherBinding
 import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.model.getSky
+
 import java.text.SimpleDateFormat
 import java.util.*
-
 
 class WeatherActivity : AppCompatActivity() {
 
     val viewModel by lazy { ViewModelProviders.of(this).get(WeatherViewModel::class.java) }
-
     private lateinit var  binding  : ActivityWeatherBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= 21) {
             val decorView = window.decorView
-            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             window.statusBarColor = Color.TRANSPARENT
-
+        }
         binding = DataBindingUtil.setContentView(this,R.layout.activity_weather)
-
+        setContentView(R.layout.activity_weather)
         if (viewModel.locationLng.isEmpty()) {
             viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
         }
-
         if (viewModel.locationLat.isEmpty()) {
             viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
         }
-
         if (viewModel.placeName.isEmpty()) {
             viewModel.placeName = intent.getStringExtra("place_name") ?: ""
         }
-
         viewModel.weatherLiveData.observe(this, Observer { result ->
             val weather = result.getOrNull()
             if (weather != null) {
@@ -57,23 +55,24 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
-            binding.swipeRefresh.isRefreshing  = false
+            binding.swipeRefresh.isRefreshing = false
         })
-
-        binding. swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+       binding. swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
         refreshWeather()
-        binding. swipeRefresh.setOnRefreshListener {
+        binding.swipeRefresh.setOnRefreshListener {
             refreshWeather()
         }
-
-        binding.includeNow.navBtn.setOnClickListener {
-            binding. drawerLayout.openDrawer(GravityCompat.START)
+        val navBtn=findViewById<Button>(R.id.navBtn)
+        navBtn.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
         }
-
         binding. drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
             override fun onDrawerStateChanged(newState: Int) {}
+
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
             override fun onDrawerOpened(drawerView: View) {}
+
             override fun onDrawerClosed(drawerView: View) {
                 val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
@@ -85,33 +84,42 @@ class WeatherActivity : AppCompatActivity() {
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
         binding.swipeRefresh.isRefreshing = true
     }
-
     fun closeDrawerLayout(){
         binding.drawerLayout.closeDrawers()
     }
-
     private fun showWeatherInfo(weather: Weather) {
-        binding.includeLifeIndex.placeName.text = viewModel.placeName
+
+        val placeName = findViewById<TextView>(R.id.placeName)
+        val currentTemp = findViewById<TextView>(R.id.currentTemp)
+        val currentSky = findViewById<TextView>(R.id.currentSky)
+        val currentAQI = findViewById<TextView>(R.id.currentAQI)
+        val forecastLayout = findViewById<View>(R.id.forecastLayout)
+        val nowLayout = findViewById<View>(R.id.nowLayout)
+
+
+        placeName.text = viewModel.placeName
         val realtime = weather.realtime
         val daily = weather.daily
         // 填充now.xml布局中数据
         val currentTempText = "${realtime.temperature.toInt()} ℃"
-        binding.includeLifeIndex.currentTemp.text = currentTempText
-        binding.includeLifeIndex.currentSky.text = getSky(realtime.skycon).info
+
+        currentTemp.text = currentTempText
+        currentSky.text = getSky(realtime.skycon).info
         val currentPM25Text = "空气指数 ${realtime.airQuality.aqi.chn.toInt()}"
-        binding.includeLifeIndex.currentAQI.text = currentPM25Text
-        binding.includeLifeIndex.nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+        currentAQI.text = currentPM25Text
+        nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
         // 填充forecast.xml布局中的数据
-        binding.includeLifeIndex.forecastLayout.removeAllViews()
+        forecastLayout.removeAllViews()
         val days = daily.skycon.size
         for (i in 0 until days) {
             val skycon = daily.skycon[i]
             val temperature = daily.temperature[i]
-            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item,
-                binding.includeLifeIndex.forecastLayout, false)
+            val view = LayoutInflater.from(this). inflate (R.layout.forecast_item, forecastLayout, false)
+
             val dateInfo = view.findViewById(R.id.dateInfo) as TextView
             val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
             val skyInfo = view.findViewById(R.id.skyInfo) as TextView
+
             val temperatureInfo = view.findViewById(R.id.temperatureInfo) as TextView
             val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
             dateInfo.text = simpleDateFormat.format(skycon.date)
@@ -120,15 +128,20 @@ class WeatherActivity : AppCompatActivity() {
             skyInfo.text = sky.info
             val tempText = "${temperature.min.toInt()} ~ ${temperature.max.toInt()} ℃"
             temperatureInfo.text = tempText
-            binding.includeLifeIndex.forecastLayout.addView(view)
+            forecastLayout.addView(view)
         }
         // 填充life_index.xml布局中的数据
         val lifeIndex = daily.lifeIndex
-        binding.includeLifeIndex.coldRiskText.text = lifeIndex.coldRisk[0].desc
-        binding.includeLifeIndex.dressingText.text = lifeIndex.dressing[0].desc
-        binding.includeLifeIndex.ultravioletText.text = lifeIndex.ultraviolet[0].desc
-        binding.includeLifeIndex.carWashingText.text = lifeIndex.carWashing[0].desc
-        binding.includeLifeIndex.weatherLayout.visibility = View.VISIBLE
+        val coldRiskText = findViewById<TextView>(R.id.coldRiskText)
+        val dressingText = findViewById<TextView>(R.id.dressingText)
+        val ultravioletText = findViewById<TextView>(R.id.ultravioletText)
+        val carWashingText = findViewById<TextView>(R.id.carWashingText)
+        val weatherLayout = findViewById<View>(R.id.weatherLayout)
+        coldRiskText.text = lifeIndex.coldRisk[0].desc
+        dressingText.text = lifeIndex.dressing[0].desc
+        ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        carWashingText.text = lifeIndex.carWashing[0].desc
+        weatherLayout.visibility = View.VISIBLE
     }
 
 }
